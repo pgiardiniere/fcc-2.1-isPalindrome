6828b43f255c4ae30941809b12213f4638a01de0
pgiardiniere <pgiardiniere@gmail.com>
2019-01-17 23:41:47 +0000

    removed faulty compare + added regex

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,20 +1,30 @@
 "use strict";
 function palindrome(str) {
-  let isPalindrome = false;
+  let isPalindrome = true;
   
   //format str
-  str = str.toLowerCase().replace(/\s/g, '');
+  str = str.toLowerCase();
+  let punctRemoved = str.replace(/,/g, "")
   //put str into an array
+console.log(str);
+console.log(punctRemoved);
   let strArray = [];
   for (let i=0; i < str.length; i++) {
     strArray.push(str.charAt(i));
   }
-  //test if array.reverse() === array
-  isPalindrome = strArray.reverse() === strArray;
+
+console.log(strArray);
+
+  for (let i=0; i<strArray.length; i++) {
+    if (strArray[i] !== strArray.reverse()[i]) {
+      isPalindrome=false;
+      break;
+    }
+  }
   console.log(isPalindrome);
   return isPalindrome;
 }
   
   
   
-palindrome("eye");
+palindrome("A man, a plan, a canal. Panama");
